c804fcfa2b75aded0974d8ce7c34311ba6774ed5
Tish <tishthegoat@gmail.com>
2024-05-05 03:08:47 -0600

    tests for input

--- tests/input.test.js
+++ tests/input.test.js
@@ -2,41 +2,68 @@ const validateLength = require("../lib/input");
 const prompts = require("../lib/input");
 const inquirer = require('inquirer')
 
-// jest.mock('inquirer')
-
-// describe('prompts', ()=>{
-// 	it('should prompt for and recieve the characters', async ()=>{
-		
-// 		inquirer.prompt = jest.fn().mockResolvedValue({letters: 'abc'})
-
-// 		await expect(prompts()).resolves.toEqual({letters: 'abc'})
-// 	})
-// })
-
-
-
-// jest.mock('inquirer')
-// describe('prompts', () =>{
-// 	it('should take in letters after prompted', async ()=>{
-// 		expect.assertions(1)
-// 		inquirer.prompt = jest.fn().mockResolvedValue({letters:'123'})
-// 		expect(prompts()).toEqual({letters: '123'})
-// 	})
-// })
-
 jest.mock('inquirer', () => {
 	return {prompt: jest.fn()}
 })
 
-describe('prompt letters', () =>{
+describe('letters prompt', () =>{
 	afterEach(()=>{
 		jest.resetAllMocks()
 	})
 	it('should take in letters after prompt', async ()=>{
 		expect(await prompts(inquirer.prompt.mockResolvedValueOnce('123'))).toBe('123')
 	})
-	// *doesn't work properly but almost there.
-	// it('should reject answer and alert to enter less than 3 characters if more than 3 entered', async() =>{
-	// 	expect(await validateLength(inquirer.prompt.mockResolvedValueOnce('1234'))).toEqual(true)
-	// })
 })
+describe('text color prompt', () =>{
+	afterEach(()=>{
+		jest.resetAllMocks()
+	})
+	it('should take in the color of choice for the letters', async ()=>{
+		expect(await prompts(inquirer.prompt.mockResolvedValueOnce('navy'))).toBe('navy')
+	})
+})
+describe('shape prompt', () =>{
+	afterEach(()=>{
+		jest.resetAllMocks()
+	})
+	it('should return the choice of shape for logo', async ()=>{
+		expect(await prompts(inquirer.prompt.mockResolvedValueOnce('triangle'))).toBe('triangle')
+	})
+})
+describe('shape color prompt', () =>{
+	afterEach(()=>{
+		jest.resetAllMocks()
+	})
+	it('should return the choice of shape color for logo', async ()=>{
+		expect(await prompts(inquirer.prompt.mockResolvedValueOnce('black'))).toBe('black')
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *doesn't work properly but almost there.
+// describe('prompt validate', () =>{
+// 	afterEach(()=>{
+// 		jest.resetAllMocks()
+// 	})
+// 	it('should reject answer and alert to enter less than 3 characters if more than 3 entered', async() =>{
+// 		expect(await validateLength('123')).toBe(false)
+// 	})
+
+// })
