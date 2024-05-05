1c5b54fb16c99695c5883b58d599995cfdc52120
Tish <tishthegoat@gmail.com>
2024-05-05 04:37:59 -0600

    classes and markup

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const prompts = require("./lib/input");
+// const prompts = require("./lib/input");
+const fs = require('fs')
+const inquirer = require('inquirer')
+// const prompts = require('./lib/input')
+const questions = require('./lib/input')
+const svgMarkUp = require('./lib/writeFile')
 
-prompts()
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, (err) =>{
+		if(err){
+			console.log('an error occured', err)
+		}
+		else{
+			console.log('file was written')
+		}
+	})
+}
+
+function init() {
+	inquirer.prompt(questions)
+	.then((data) => {
+		console.log(data)
+		
+		writeToFile('examples/example.svg',svgMarkUp(data))
+	})
+}
+init()
