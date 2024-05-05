ce1bf84f61dd9651b2e744be1248138a0a04d562
Tish <tishthegoat@gmail.com>
2024-05-05 17:28:55 -0600

    triangle test w/ color

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,26 +1,28 @@
 // const { blue } = require("colors")
 const Triangle = require("../lib/shapes")
 const Shape = require("../lib/shapes")
+const Square = require("../lib/shapes")
 
 describe('shape class', () =>{
 	it('should return a color selected', ()=>{
 		const shape = new Shape()
 		expect(shape.setColor('blue')).toBe('blue')
 	})
-	it('should fill the string with the color selected', ()=>{
-		const shape = new Shape()
-		shape.setColor('blue')
-		expect(shape.render()).toEqual(`<polygon points = "150, 18 244, 182 56, 182" fill="blue"/>`)
+})
+
+describe('triangle shape', () =>{
+	const shape = new Triangle
+	shape.setColor('blue')
+	it('should render a triangle with a blue fill', ()=>{
+		expect(shape.render()).toEqual('<polygon points = "150, 18 244, 182 56, 182" fill="blue"/>')
 	})
 })
 
-
-
-// describe('triangle shape', () =>{
-// 	const shape = new Triangle
-// 	shape.setColor('blue')
-// 	it('should render a triangle with a blue fill', ()=>{
-// 		expect(shape.render()).toEqual('<polygon points = "150, 18 244, 182 56, 182" fill="blue"/>')
+// describe('square shape', () =>{
+// 	const shape = new Square
+// 	shape.setColor('green')
+// 	it('should return a string to render the shape and color of logo',()=>{
+// 		expect(shape.render()).toEqual(`<rect x="150" y="150" width="30" height="30" fill="green"/>`)
 // 	})
 // })
 
