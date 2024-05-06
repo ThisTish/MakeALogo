785192d0bfa38220203021c3c7802a3554cb4588
Tish <tishthegoat@gmail.com>
2024-05-05 22:58:51 -0600

    all shape tests

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,7 +1,4 @@
-// const { blue } = require("colors")
-const Triangle = require("../lib/shapes")
-const Shape = require("../lib/shapes")
-const Square = require("../lib/shapes")
+const { Shape, Triangle, Square, Circle} = require("../lib/shapes")
 
 describe('shape class', () =>{
 	it('should return a color selected', ()=>{
@@ -18,11 +15,19 @@ describe('triangle shape', () =>{
 	})
 })
 
-// describe('square shape', () =>{
-// 	const shape = new Square
-// 	shape.setColor('green')
-// 	it('should return a string to render the shape and color of logo',()=>{
-// 		expect(shape.render()).toEqual(`<rect x="150" y="150" width="30" height="30" fill="green"/>`)
-// 	})
-// })
+describe('square shape', () =>{
+	const shape = new Square
+	shape.setColor('green')
+	it('should return a string to render a green square',()=>{
+		expect(shape.render()).toEqual(`<rect x="150" y="150" width="30" height="30" fill="green"/>`)
+	})
+})
+
+describe('circle shape', () =>{
+	const shape = new Circle
+	shape.setColor('yellow')
+	it('should return a string to render a yellow circle',()=>{
+		expect(shape.render()).toEqual(`<circle cx="25" cy="75" r="20" fill="yellow"/>`)
+	})
+})
 
